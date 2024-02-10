90415cf66bacc9756c1f1028f8efb2c9dd40cd93
Dhruv <dhruvrg2003@gmail.com>
2024-02-10 17:27:54 +0530

    added profic pic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import { useEffect } from "react";
 import "./App.css";
-import GoogleAuth from "./auth/GoogleAuth";
 import Navbar from "./common/Navbar";
+import useAuthModal from "./hooks/useAuthModal";
 import Contact from "./pages/Contact";
+import Temp from "./common/Temp";
 
 function App() {
+  const authModal = useAuthModal();
+
+  useEffect(() => {
+    if (localStorage.getItem("id")) {
+      authModal.onOpen();
+      console.log("llll");
+    }
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
-      <GoogleAuth />
       {/* <Contact /> */}
+      <Temp />
     </div>
   );
 }
--- src/auth/GoogleAuth.jsx
+++ src/auth/GoogleAuth.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { useGoogleLogin } from "@react-oauth/google";
 import axios from "axios";
+import useAuthModal from "../hooks/useAuthModal";
 
 const GoogleAuth = () => {
   const [user, setUser] = useState([]);
   const [profile, setProfile] = useState([]);
+  const authModal = useAuthModal();
 
   const login = useGoogleLogin({
     onSuccess: (codeResponse) => setUser(codeResponse),
@@ -23,7 +25,6 @@ const GoogleAuth = () => {
         }
       );
       setProfile(res.data);
-      console.log(res.data);
     } catch (error) {
       console.log(error);
     }
@@ -35,10 +36,49 @@ const GoogleAuth = () => {
     }
   }, [user]);
 
+  const getToken = async (name, email, picture) => {
+    console.log(name, email, picture);
+    try {
+      const res = await axios.post("https://api.example.com/data", {
+        name,
+        email,
+        picture,
+      });
+      console.log(res.data);
+      localStorage.setItem("id", res);
+      authModal.onOpen();
+    } catch (error) {
+      console.error("Error", error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (profile.length != 0) {
+      getToken({
+        name: profile.name,
+        email: profile.email,
+        picture: profile.picture,
+      });
+    }
+  }, [profile]);
+
   return (
-    <div>
-      <button onClick={() => login()}>Google</button>
-    </div>
+    <>
+      {profile.length != 0 ? (
+        <div className="flex items-center gap-3">
+          <img
+            className="w-10 h-10 rounded-full"
+            src={profile.picture}
+            alt="image"
+          />
+          <p className="font-semibold">
+            {profile.name.charAt(0).toUpperCase() + profile.name.slice(1)}
+          </p>
+        </div>
+      ) : (
+        <button onClick={() => login()}>Google</button>
+      )}
+    </>
   );
 };
 export default GoogleAuth;
