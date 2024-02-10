5e788313f363828c05339398086b818f563a1f6b
Horror26 <1032210438@tcetmumbai.in>
2024-02-10 17:44:52 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 import GoogleAuth from "./auth/GoogleAuth";
 import Navbar from "./common/Navbar";
 import Contact from "./pages/Contact";
-
+import Footer from "./common/Footer";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <GoogleAuth />
       {/* <Contact /> */}
+      <Footer/>
     </div>
   );
 }
